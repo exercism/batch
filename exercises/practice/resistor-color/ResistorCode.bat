a1ac566274a9739451ee33150837f8c2beb91638
Murat Kirazkaya <groophylifefor@gmail.com>
2024-07-22 12:35:19 +0300

    fix: format fix

--- exercises/practice/resistor-color/ResistorCode.bat
+++ exercises/practice/resistor-color/ResistorCode.bat
@@ -2,30 +2,9 @@
 setlocal enabledelayedexpansion
 
 set colorCode=%~1
+set "result="
 
-if not defined colorCode (
-    for %%a IN (black brown red orange yellow green blue violet gray white) DO (
-      echo %%~a
-    )
-    exit /b 1
-)
+REM Your code goes here
 
-set "colors[black]=0"
-set "colors[brown]=1"
-set "colors[red]=2"
-set "colors[orange]=3"
-set "colors[yellow]=4"
-set "colors[green]=5"
-set "colors[blue]=6"
-set "colors[violet]=7"
-set "colors[gray]=8"
-set "colors[white]=9"
-
-if not defined colors[%colorCode%] (
-    echo invalid color
-    exit /b 1
-)
-
-set "result=!colors[%colorCode%]!"
 
 echo %result%
