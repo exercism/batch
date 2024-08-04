3386d2a8e10911f6e64acd4766735ed8a1ab9d25
Murat Kirazkaya <groophylifefor@gmail.com>
2024-08-04 13:27:03 +0300

    windows implementation

--- bin/test-runner.bat
+++ bin/test-runner.bat
@@ -1,9 +1,39 @@
 @echo off
+setlocal EnableDelayedExpansion
 
-set slug=%~1
-set processed_name=%~2
+cd..
+set configPath=config.json
 
-cd exercises
-cd practice
-cd %slug%
-call %processed_name%Test.bat test-runner
+for /f "usebackq tokens=*" %%a in ("%configPath%") do (
+    set "line=%%a"
+    
+    REM Look for lines containing the slug
+    echo !line! | findstr /c:"\"slug\"" >nul
+    if !errorlevel! equ 0 (
+        set "_slug=!line:*\"slug\": \"=!"
+        set "_slug=!_slug:\"",=!"
+        set "_slug=!_slug:\"",=!"
+    )
+
+    REM Look for lines containing the name
+    echo !line! | findstr /c:"\"name\"" >nul
+    if !errorlevel! equ 0 (
+        set "_name=!line:*\"name\": \"=!"
+        set "_name=!_name:\"",=!"
+        set "_name=!_name:\"",=!"
+        for /f "tokens=2 delims=:" %%a in ('echo !_slug!') do set "slug=%%a"
+        set "slug=!slug:~2,-2!"
+        for /f "tokens=2 delims=:" %%a in ('echo !_name!') do set "name=%%a"
+        set "name=!name:~2,-2!"
+
+
+        echo Slug: !slug!
+        echo Name: !name!
+        echo Exec: call exercises/practice/!slug!/!name: =!Test.bat test-runner
+        call exercises/practice/!slug!/!name: =!Test.bat test-runner
+        echo.
+        
+    )
+)
+
+endlocal
